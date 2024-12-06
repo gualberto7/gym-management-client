dc94f65a967ca9528bb810f57d2aa9b8b585a873
gcuiza <gualbertocuizav@gmail.com>
2024-12-06 10:26:05 -0400

    Base login form

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,20 @@
+<script lang="ts" setup>
+defineProps({
+  title: {
+    type: String,
+    required: false,
+    default: "",
+  },
+});
+</script>
+
 <template>
   <div class="bg-white rounded-lg shadow-lg p-6">
+    <div v-if="title" class="mb-5 text-center">
+      <h2 class="text-xl font-semibold text-gray-900 dark:text-white">
+        {{ title }}
+      </h2>
+    </div>
     <slot></slot>
   </div>
 </template>
