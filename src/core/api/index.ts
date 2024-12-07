4cdb353068b28411746ac1bd731c9f281efbc9fd
gcuiza <gualbertocuizav@gmail.com>
2024-12-07 14:18:47 -0400

    Api setup

---
+++ src/core/api/index.ts
@@ -0,0 +1,23 @@
+import axiosLib from 'axios'
+import Cookies from 'js-cookie'
+
+const axios = axiosLib.create({
+  baseURL: 'http://localhost:8000/',
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+    'Accept': 'application/json',
+  },
+    withCredentials: true,
+    xsrfCookieName: 'XSRF-TOKEN',
+})
+
+/*axios.interceptors.request.use(async (config) => {
+  if ((config.method as string).toLowerCase() !== 'get') {
+    await axios.get('/csrf-cookie').then()
+    config.headers['X-XSRF-TOKEN'] = Cookies.get('XSRF-TOKEN')
+  }
+
+  return config
+})*/
+
+export default axios
