13ac77d1cfb25ae074d07c5c52f8600d7e464cd0
gcuiza <gualbertocuizav@gmail.com>
2024-12-08 14:46:19 -0400

    Button component

---
+++ src/core/components/Button.vue
@@ -0,0 +1,63 @@
+<script lang="ts" setup>
+import { computed, type PropType } from "vue";
+
+const props = defineProps({
+  color: {
+    type: String as PropType<
+      "primary" | "secondary" | "info" | "danger" | "warning"
+    >,
+    default: "primary",
+  },
+  size: {
+    type: String,
+    default: "md",
+  },
+  type: {
+    type: String as PropType<"button" | "submit" | "reset">,
+    default: "button",
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const styles = computed(() => {
+  return {
+    disabled: props.disabled,
+  };
+});
+</script>
+
+<template>
+  <button
+    class="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+    :class="[color, size, styles]"
+    :type="type"
+    :disabled="disabled"
+  >
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.primary {
+  @apply bg-blue-700 hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700;
+}
+.secondary {
+  @apply bg-gray-700 hover:bg-gray-800 dark:bg-gray-600 dark:hover:bg-gray-700;
+}
+.info {
+  @apply bg-green-700 hover:bg-green-800 dark:bg-green-600 dark:hover:bg-green-700;
+}
+.danger {
+  @apply bg-red-700 hover:bg-red-800 dark:bg-red-600 dark:hover:bg-red-700;
+}
+.warning {
+  @apply bg-yellow-700 hover:bg-yellow-800 dark:bg-yellow-600 dark:hover:bg-yellow-700;
+}
+
+.disabled {
+  @apply bg-gray-400 hover:bg-gray-400 cursor-not-allowed;
+}
+</style>
