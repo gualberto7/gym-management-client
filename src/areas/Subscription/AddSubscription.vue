5ce34723365aea81f1903cdd9deead36f7088f61
gcuiza <gualbertocuizav@gmail.com>
2024-12-11 17:56:15 -0400

    add columns

--- src/areas/Subscription/AddSubscription.vue
+++ src/areas/Subscription/AddSubscription.vue
@@ -12,12 +12,20 @@ const memberships = computed(() => gymStore.memberships);
 <template>
   <div>
     <PageHeader title="Registro" />
-    <div class="flex gap-5">
-      <MembershipCard
-        v-for="membership in memberships"
-        :key="membership.id"
-        :membership="membership"
-      />
+    <div class="grid grid-cols-2">
+      <div class="text-center">
+        <h3 class="text-lg mb-3">Registrar</h3>
+      </div>
+      <div class="text-center">
+        <h3 class="text-lg mb-3">Planes disponibles</h3>
+        <div class="flex gap-5">
+          <MembershipCard
+            v-for="membership in memberships"
+            :key="membership.id"
+            :membership="membership"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
