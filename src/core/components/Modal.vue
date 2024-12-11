058eb60bdcb0282eef3a32fe690d94a4d8851784
gcuiza <gualbertocuizav@gmail.com>
2024-12-11 19:46:49 -0400

    make modal static

--- src/core/components/Modal.vue
+++ src/core/components/Modal.vue
@@ -8,17 +8,18 @@ const { modal, hide } = useModalStore();
 
 onMounted(() => {
   if (modalRef.value) {
-    modal.modalInstance = new Modal(modalRef.value);
+    modal.modalInstance = new Modal(modalRef.value, { backdrop: "static" });
   }
 });
 </script>
 
 <template>
   <div
-    id="default-modal"
-    ref="modalRef"
+    id="static-modal"
+    data-modal-backdrop="static"
     tabindex="-1"
     aria-hidden="true"
+    ref="modalRef"
     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
   >
     <div class="relative p-4 w-full max-w-2xl max-h-full">
