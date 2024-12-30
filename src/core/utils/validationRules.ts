13a2dd47802c57c2d1309e53e792e935ac2ffc1a
gcuiza <gualbertocuizav@gmail.com>
2024-12-30 10:36:07 -0400

    fix required validation rule

--- src/core/utils/validationRules.ts
+++ src/core/utils/validationRules.ts
@@ -3,45 +3,53 @@
     This file contains validation rules that are used in the form validation process.
 */
 
-import { type ValidationResult } from '../interfaces/ValidationRules'
+import { type ValidationResult } from "../interfaces/ValidationRules";
 
 const required = (value: any): ValidationResult => {
-  if (value.trim() !== '') return { isValid: true, errorMessage: null }
+  value = value.toString();
+  if (value && value.trim() !== "")
+    return { isValid: true, errorMessage: null };
 
-  return { isValid: false, errorMessage: 'This field is required' }
-}
+  return { isValid: false, errorMessage: "This field is required" };
+};
 
 const string = (value: any): ValidationResult => {
-  if (typeof value === 'string') return { isValid: true, errorMessage: null }
+  if (typeof value === "string") return { isValid: true, errorMessage: null };
 
-  return { isValid: false, errorMessage: 'This field must be a string' }
-}
+  return { isValid: false, errorMessage: "This field must be a string" };
+};
 
 const email = (value: any): ValidationResult => {
-  const re = /\S+@\S+\.\S+/
-  if (re.test(value)) return { isValid: true, errorMessage: null }
+  const re = /\S+@\S+\.\S+/;
+  if (re.test(value)) return { isValid: true, errorMessage: null };
 
-  return { isValid: false, errorMessage: 'This field must be a valid email' }
-}
+  return { isValid: false, errorMessage: "This field must be a valid email" };
+};
 
 const min = (value: any, length: any): ValidationResult => {
-  if (value.length >= length) return { isValid: true, errorMessage: null }
+  if (value.length >= length) return { isValid: true, errorMessage: null };
 
-  return { isValid: false, errorMessage: `This field must have at least ${length} characters` }
-}
+  return {
+    isValid: false,
+    errorMessage: `This field must have at least ${length} characters`,
+  };
+};
 
 const max = (value: any, length: any): ValidationResult => {
-  if (value.length <= length) return { isValid: true, errorMessage: null }
+  if (value.length <= length) return { isValid: true, errorMessage: null };
 
-  return { isValid: false, errorMessage: `This field must have less than ${length} characters` }
-}
+  return {
+    isValid: false,
+    errorMessage: `This field must have less than ${length} characters`,
+  };
+};
 
 const validationRules = {
   required,
   string,
   email,
   min,
-  max
-}
+  max,
+};
 
-export default validationRules
+export default validationRules;
