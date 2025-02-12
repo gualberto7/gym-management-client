d93a29c4e81d6b705bfa67f97a310dde0f73131d
gcuiza <gualbertocuizav@gmail.com>
2025-02-12 19:53:07 -0400

    setup toast notifications

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import './assets/main.css'
-import { registerPlugins } from './core/plugins'
+import "./assets/main.css";
+import { registerPlugins } from "./core/plugins";
+import Vue3Toastify, { type ToastContainerOptions, toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
-import App from './App.vue'
+import App from "./App.vue";
 
-const app = createApp(App)
+const app = createApp(App);
 
-registerPlugins(app)
+registerPlugins(app);
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+  position: toast.POSITION.BOTTOM_CENTER,
+} as ToastContainerOptions);
 
-app.mount('#app')
+app.mount("#app");
