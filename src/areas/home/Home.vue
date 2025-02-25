aedd304b63545d469df29dc31b6e00164aeebc54
gcuiza <gualbertocuizav@gmail.com>
2025-02-25 09:49:20 -0400

    Home search member

--- src/areas/home/Home.vue
+++ src/areas/home/Home.vue
@@ -1,14 +1,40 @@
 <script lang="ts" setup>
-import { useModalStore } from "@/core/store/modal";
+// Imports ----
+import { useGymStore } from "../gym/store/gymStore";
+import FindMember from "../member/classes/FindMember";
 
-const { show } = useModalStore();
+// Components ----
+import SearchInput from "@/core/components/SearchInput.vue";
+import SearchContainer from "@/core/components/SearchContainer.vue";
+
+// State ----
+const gymStore = useGymStore();
+const findMember = new FindMember();
+
+// Methods ----
+const handleResponse = (data: any) => {
+  console.log(data);
+};
 </script>
 
 <template>
-  <div>
+  <div class="home-page">
     <PageHeader title="Inicio" />
-    <button @click="show('SubscriptionInfoModal', 'Subscription', {})">
-      Open Subscription Info Modal
-    </button>
+    <div class="h-full flex flex-col justify-center items-center gap-10">
+      <h1 class="text-2xl font-bold text-gray-600 tracking-widest">
+        Hola, bienvenido al Gimnasio {{ gymStore.currentGym?.name }}
+      </h1>
+      <div class="w-1/2">
+        <SearchContainer :search="findMember">
+          <SearchInput @on-response="handleResponse" />
+        </SearchContainer>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.home-page {
+  height: 75vh;
+}
+</style>
