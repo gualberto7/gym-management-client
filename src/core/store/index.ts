2273c38f1c1007b0db775608f2672db2ef5b8268
gcuiza <gualbertocuizav@gmail.com>
2025-03-09 13:35:21 -0400

    add event bus feature

--- src/core/store/index.ts
+++ src/core/store/index.ts
@@ -3,11 +3,13 @@ import { defineStore } from "pinia";
 import { useGymStore } from "@/areas/gym/store/gymStore";
 import { useSessionStore } from "@/areas/auth/store/sessionStore";
 import { useModalStore } from "./modal";
+import eventBus from "./eventBus";
 
 export const useAppStore = defineStore("app", {
   state: () => ({
     user: useSessionStore(),
     gym: useGymStore(),
     modal: useModalStore(),
+    event: eventBus,
   }),
 });
